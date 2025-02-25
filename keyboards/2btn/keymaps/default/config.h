015837e66b01356bdf0d0c2eea3f846b8057e454
Wilson Ho <Wilson.Ho@amd.com>
2025-02-25 13:11:19 -0800

    Added custom 2-button configuration: 2btn

---
+++ keyboards/2btn/keymaps/default/config.h
@@ -0,0 +1,35 @@
+/*
+This is the c configuration file for the keymap
+
+Copyright 2012 Jun Wako <wakojun@gmail.com>
+Copyright 2015 Jack Humbert
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+//#define USE_MATRIX_I2C
+
+//#define QUICK_TAP_TERM 0
+//#define TAPPING_TERM 100
+
+#define MATRIX_ROWS 1
+#define MATRIX_COLS 2
+
+#define DIODE_DIRECTION ROW2COL
+
+#define VENDOR_ID  0x0000 // Replace with your vendor ID  
+#define PRODUCT_ID 000000 // Replace with your product ID  
+#define DEVICE_VER 0x0001 // Replace with your device version  
